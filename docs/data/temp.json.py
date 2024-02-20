0db554414f539f4dd80c8d3d72a49a022864246c
Vikas Negi <vikas.negi10@gmail.com>
2024-02-20 20:29:31 +0100

    Add logic to get app. temp.

--- docs/data/temp.json.py
+++ docs/data/temp.json.py
@@ -3,14 +3,19 @@ import sys
 from helpers import get_response
 
 data = get_response("temperature_2m")
-
-temp = data["hourly"]["temperature_2m"]
 time = data["hourly"]["time"]
+temp = data["hourly"]["temperature_2m"]
+
+data = get_response("apparent_temperature")
+app_temp = data["hourly"]["apparent_temperature"]
+
+assert len(temp) == len(app_temp), "Length mismatch between lists!"
 
 forecast_dict = []
 
 for i in range(len(temp)):
     forecast_dict.append({'time' : time[i],
-                          'temp' : temp[i]})
+                          'temp' : temp[i],
+                          'app_temp' : app_temp[i]})
 
 json.dump(forecast_dict, sys.stdout)
